2d690627007bb00cdf74b1e202272535c2be7867
cengiztuglu <cngztuglu@gmail.com>
2023-12-29 16:00:14 +0300

    AddTextReceiver

--- receiver.py
+++ receiver.py
@@ -1,64 +1,84 @@
-import socket
 from Crypto.Cipher import AES
+import socket
 import pickle
-from tkinter import Tk, Label
+from tkinter import Tk, Text,Label, Toplevel
 from PIL import Image, ImageTk
+import io
+
+decrypted_image = None  # Resmi saklamak için global değişken
 
 def decrypt_image(encrypted_data, key):
+    global decrypted_image
+    
     nonce, tag, ciphertext = encrypted_data
-    
-    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
-    decrypted_image = cipher.decrypt_and_verify(ciphertext, tag)
-    
-    with open('received_image.jpg', 'wb') as file:
-        file.write(decrypted_image)
-    
-    print("Resim başarıyla çözüldü.")
-    show_decrypted_image('received_image.jpg')
 
-def receive_encrypted_image():
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    decrypted_image_data = cipher.decrypt_and_verify(ciphertext, tag)
+
+    with open('received_image.jpg', 'wb') as file:
+        file.write(decrypted_image_data)
+
+    print("Resim başarıyla çözüldü.")
+    decrypted_image = Image.open('received_image.jpg')
+    decrypted_image.thumbnail((300, 300))  # Resmi boyutlandırma
+    photo = ImageTk.PhotoImage(decrypted_image)
+
+    return photo
+
+def decrypt_text(encrypted_data, key):
+    nonce, tag, ciphertext = encrypted_data
+
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    decrypted_text = cipher.decrypt_and_verify(ciphertext, tag)
+
+    return decrypted_text.decode('utf-8')
+
+def show_decrypted_data(image, text):
+    top = Toplevel()
+    top.title("Çözülmüş Veri")
+    window_width = 400
+    window_height = 350
+
+    screen_width = top.winfo_screenwidth()
+    screen_height = top.winfo_screenheight()
+    x_coordinate = (screen_width - window_width) // 2
+    y_coordinate = (screen_height - window_height) // 2
+
+    top.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
+
+    image_label = Label(top, image=image)
+    image_label.pack()
+
+    text_box = Text(top, height=5, width=50)
+    text_box.pack()
+    text_box.insert("1.0", text)
+
+    top.mainloop()
+
+def receive_encrypted_data():
     HOST = '127.0.0.1'
     PORT = 8910
-    
+
+    root = Tk()
+    root.withdraw()
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         conn, addr = s.accept()
-        with conn:
-            print(f'{addr} tarafından bağlantı sağlandı')
-         
-            serialized_data = b''  
+
+        while True:
+            serialized_data = b''
             while True:
                 chunk = conn.recv(4096)
                 if not chunk:
                     break
                 serialized_data += chunk
-            
+
             received_data = pickle.loads(serialized_data)
-            decrypt_image(*received_data)
+            decrypted_image = decrypt_image(received_data[0], received_data[2])  # Resmi çöz
+            decrypted_text = decrypt_text(received_data[1], received_data[2])  # Metni çöz
+            print("Alınan şifrelenmiş metin:", decrypted_text)
+            show_decrypted_data(decrypted_image, decrypted_text)  # Çözülen resmi ve metni göster
 
-def show_decrypted_image(image_path):
-    root = Tk()
-    root.title("Çözülmüş Resim")
-    window_width = 400
-    window_height = 300
-
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    x_coordinate = (screen_width - window_width) // 2
-    y_coordinate = (screen_height - window_height) // 2
-
-    root.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
-
-
-    image = Image.open(image_path)
-    photo = ImageTk.PhotoImage(image)
-
-    label = Label(root, image=photo)
-    label.image = photo
-    label.pack()
-
-    root.mainloop()
-
-# Program başladığında resmi çözüp göster
-receive_encrypted_image()
+receive_encrypted_data()
--- sender.py
+++ sender.py
@@ -1,7 +1,7 @@
-import socket
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from tkinter import Tk, Button, filedialog
+from tkinter import Tk, Button,filedialog, Text
+import socket
 import pickle
 
 def encrypt_image(input_file, key):
@@ -13,15 +13,20 @@ def encrypt_image(input_file, key):
     
     return cipher.nonce, tag, ciphertext
 
-def send_encrypted_image(image_data, key):
+def encrypt_text(plain_text, key):
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(plain_text.encode('utf-8'))
+    return cipher.nonce, tag, ciphertext
+
+def send_encrypted_data(image_data, text_data, key):
     HOST = '127.0.0.1'
     PORT = 8910
     
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        serialized_data = pickle.dumps((image_data, key))
+        serialized_data = pickle.dumps((image_data, text_data, key))
         s.sendall(serialized_data)
-        print("Şifrelenmiş resim gönderildi.")
+        print("Şifrelenmiş resim ve metin gönderildi.")
 
 def on_button_click():
     root = Tk()
@@ -31,13 +36,23 @@ def on_button_click():
     
     if file_path:
         key = get_random_bytes(16)
+        
+        # Resmi şifrele ve gönder
         encrypted_image_data = encrypt_image(file_path, key)
-        send_encrypted_image(encrypted_image_data, key)
+        
+        # TextBox'tan metni al
+        text = text_box.get("1.0", "end-1c")
+        
+        # Metni şifrele
+        encrypted_text_data = encrypt_text(text, key)
+        
+        # Resmi ve metni gönder
+        send_encrypted_data(encrypted_image_data, encrypted_text_data, key)
         print("Şifrelenmiş veri:", encrypted_image_data)
 
 # Buton oluştur
 root = Tk()
-root.title("Resmi Şifreli Gönder")
+root.title("Resmi ve Metni Şifreli Gönder")
 window_width = 400
 window_height = 300
 
@@ -48,9 +63,11 @@ y_coordinate = (screen_height - window_height) // 2
 
 root.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 
-
+text_box = Text(root, height=5, width=50)
+text_box.pack()
 
 button = Button(root, text="Resim Seç ve Gönder", command=on_button_click)
 button.pack()
 
 root.mainloop()
+y
